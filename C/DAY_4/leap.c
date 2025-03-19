9d163984afdf5cfc46ded9d899d368848809e1a3
Vikash Gupta <vikash9c35@gmail.com>
2025-03-19 19:53:11 +0530

    Update leap.c

--- C/DAY_4/leap.c
+++ C/DAY_4/leap.c
@@ -4,7 +4,7 @@ void main()
     int yr;
     printf("Enter the year:  ");
     scanf("%d",&yr);
-    if(yr%4==0 || yr%100==0 || yr%400==0)
+    if((yr % 400 == 0) || (yr % 4 == 0 && yr % 100 != 0))
     printf("The given year is leap year: %d\n",yr);
     else
     printf("The given year is  not a leap year: %d\n");

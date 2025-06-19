2b142f8832bc5ee5e6067ad1d9642dee50011009
Vikash Gupta <vikash9c35@gmail.com>
2025-06-19 14:26:33 +0530

    All Done !!

--- C/ASSIGNMENT13/Q4.c
+++ C/ASSIGNMENT13/Q4.c
@@ -1,5 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
 
-void abc(_){
-    
+// Define a structure
+struct Person {
+    char name[50];
+    int age;
+};
+
+// Function to accept user input
+void getInput(struct Person *p) {
+    printf("Enter name: ");
+    scanf("%s", p->name);
+    printf("Enter age: ");
+    scanf("%d", &p->age);
 }
+
+// Function to print structure members
+void print(struct Person p) {
+    printf("Name: %s\n", p.name);
+    printf("Age: %d\n", p.age);
+}
+
+int main() {
+    struct Person person;
+
+    // Get user input
+    getInput(&person);
+
+    // Print structure members
+    print(person);
+
+    return 0;}
